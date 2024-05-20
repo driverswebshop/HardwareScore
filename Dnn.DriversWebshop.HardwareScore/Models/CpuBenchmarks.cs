9e822882e4de2324fa977d4910216a671d2c3116
Dang3rK1LL <damjan.dani@gmail.com>
2024-05-20 20:35:19 +0200

    Created SQL prodedures, updated the install and uninstall SqlDataProvider and created the CpuBenchmark.cs class

---
+++ Dnn.DriversWebshop.HardwareScore/Models/CpuBenchmarks.cs
@@ -0,0 +1,7 @@
+﻿public class CpuBenchmarks
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int score { get; set; }
+    public float percentage { get; set; }
+}
