58db147f25d23edd8c06adf5d58cad25d8264b38
Dang3rK1LL <damjan.dani@gmail.com>
2024-05-20 22:49:16 +0200

    Created the CpuController class and made changed to the Index.cshtml

---
+++ Dnn.DriversWebshop.HardwareScore/Controllers/CpuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Data;
+
+namespace DriversWebshop.Dnn.Dnn.DriversWebshop.HardwareScore.Controllers
+{
+    public class CpuController : Controller
+    {
+        // GET: Cpu
+        public IList<Task> GetTasks(string name)
+        {
+            return CBO.FillCollection<Task>(DataProvider.Instance().ExecuteReader("Cpu_GetPercentage", name));
+        }
+    }
+}
